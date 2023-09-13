221444d7636d09322fd80335e0a10b3b638c725c
Dischead <84547157+DischeadG@users.noreply.github.com>
2023-09-12 23:37:43 -0600

    Add files via upload

---
+++ Calculadora - Copy/Calculadora/resource.h
@@ -0,0 +1,46 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Calculadora.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CALCULADORA_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define masmenos                        1001
+#define cero                            1002
+#define punto                           1003
+#define igual                           1004
+#define uno                             1017
+#define dos                             1018
+#define tres                            1019
+#define mas                             1020
+#define cuatro                          1021
+#define cinco                           1022
+#define seis                            1023
+#define menos                           1024
+#define siete                           1025
+#define ocho                            1026
+#define nueve                           1027
+#define multiplicacion                  1028
+#define unosobrex                       1029
+#define potencia                        1030
+#define raiz                            1031
+#define division                        1032
+#define IDC_BUTTON21                    1033
+#define residuo                         1033
+#define clear                           1034
+#define clearone                        1035
+#define deleterealnofake                1036
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
